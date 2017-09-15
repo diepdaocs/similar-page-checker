8d20d4e4ddda7272d9ae2be9473f46de7f7f7ef2
Diep Dao <diepdao12892@gmail.com>
2017-09-16 00:43:45 +0700

    Reduce timeout worker

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ export PYTHONPATH=/root/webpages-duplicated-checking
 HOST=0.0.0.0
 PORT=8888
 fuser -k -n tcp $PORT
-/home/root/virtualenvs/webpages-duplicated-checking/bin/python /home/root/virtualenvs/webpages-duplicated-checking/bin/gunicorn -k tornado -w 2 -b $HOST:$PORT main:app --max-requests 10000 --timeout 3600
+/home/root/virtualenvs/webpages-duplicated-checking/bin/python /home/root/virtualenvs/webpages-duplicated-checking/bin/gunicorn -k tornado -w 2 -b $HOST:$PORT main:app --max-requests 10000 --timeout 60
